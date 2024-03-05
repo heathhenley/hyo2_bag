c5b6e89ca424eea872199861e272a061a7534be1
Heath Henley <heath.henley@farsounder.com>
2024-03-05 15:11:18 -0500

    all metadata examples

--- examples/bag_metadata_vertical_datum.py
+++ examples/bag_metadata_vertical_datum.py
@@ -1,21 +1,21 @@
 import os
 import logging
 
-from hyo2.abc.lib.logging import set_logging
 from hyo2.bag.bag import BAGFile
 from hyo2.bag.helper import Helper
 from hyo2.bag.meta import Meta
 
-set_logging(ns_list=['hyo2.bag'])
-logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.DEBUG)
 
 file_bag_0 = os.path.join(Helper.samples_folder(), "bdb_01.bag")
-if os.path.exists(file_bag_0):
-    logger.debug("- file_bag_0: %s" % file_bag_0)
+if not os.path.exists(file_bag_0):
+    raise FileNotFoundError(f"the file does not exist: {file_bag_0}")
+logging.debug("- file_bag_0:\n{file_bag_0}")
 
 bag_0 = BAGFile(file_bag_0, mode='r')
-logger.debug(bag_0)
+logging.debug(bag_0)
 
 meta = Meta(bag_0.metadata())
 
-logger.debug("vertical datum: %s (%s)" % (meta.wkt_vertical_datum, type(meta.wkt_vertical_datum)))
+logging.debug(
+    f"vertical datum: {meta.wkt_vertical_datum} ({type(meta.wkt_vertical_datum)})")
