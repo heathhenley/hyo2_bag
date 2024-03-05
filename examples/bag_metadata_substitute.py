29ecb292227fae23b00b6aa757d5d02554012e6f
Heath Henley <heath.henley@farsounder.com>
2024-03-05 14:47:52 -0500

    unused and minor tweaks

--- examples/bag_metadata_substitute.py
+++ examples/bag_metadata_substitute.py
@@ -2,27 +2,25 @@ import os
 import logging
 from shutil import copyfile
 
-from hyo2.abc.lib.logging import set_logging
 from hyo2.bag.bag import BAGFile
 from hyo2.bag.helper import Helper
-from hyo2.bag.meta import Meta
 
-set_logging(ns_list=['hyo2.bag'])
-logger = logging.getLogger(__name__)
+logging.getLogger(__name__).setLevel(logging.DEBUG)
 
 file_bag_0 = os.path.join(Helper.samples_folder(), "bdb_01.bag")
-if os.path.exists(file_bag_0):
-    logger.debug("- file_bag_0: %s" % file_bag_0)
+if not os.path.exists(file_bag_0):
+    raise FileNotFoundError(f"the file does not exist: {file_bag_0}")
+logging.debug(f"- file_bag_0:\n{file_bag_0}")
 
 file_bag_copy = os.path.join(os.path.dirname(__file__), "tmp_copy.bag")
 bag_copy = copyfile(file_bag_0, file_bag_copy)
 
 bag_0 = BAGFile(file_bag_copy, mode="r+")
-logger.debug(bag_0)
+logging.debug(bag_0)
 
 output_xml = "fixed_metadata.xml"
 if not os.path.exists(output_xml):
-    raise RuntimeError("unable to find metadata file: %s" % output_xml)
+    raise FileNotFoundError(f"unable to find metadata file: {output_xml}")
 bag_0.substitute_metadata(output_xml)
 os.remove(output_xml)
 
