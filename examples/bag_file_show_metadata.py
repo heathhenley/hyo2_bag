7a2baa4e207113bd65f51c842fef05264c35e3bb
Heath Henley <heath.henley@farsounder.com>
2024-03-05 14:43:05 -0500

    First three examples running (python 3.12)

--- examples/bag_file_show_metadata.py
+++ examples/bag_file_show_metadata.py
@@ -1,21 +1,17 @@
-import os
 import logging
+import os
 
-from hyo2.abc.lib.logging import set_logging
 from hyo2.bag.bag import BAGFile
 from hyo2.bag.helper import Helper
-from hyo2.bag.meta import Meta
 
-set_logging(ns_list=['hyo2.bag'])
-logger = logging.getLogger(__name__)
+logging.getLogger().setLevel(logging.DEBUG)
 
-# file_bag_0 = os.path.join(Helper.samples_folder(), "bdb_01.bag")
-# file_bag_0 = r"D:\google_drive\_ccom\QC Tools\data\survey\BAG Checks\ResolutionCheck\F00799_MB_2m_MLLW_1of2.bag"
-file_bag_0 = r"C:\code\cpp\bag\examples\sample-data\NAVO_data\JD211_public_Release_1-4_UTM.bag"
-if os.path.exists(file_bag_0):
-    logger.debug("- file_bag_0: %s" % file_bag_0)
+file_bag_0 = os.path.join(Helper.samples_folder(), "bdb_01.bag")
+if not os.path.exists(file_bag_0):
+  raise FileNotFoundError(f"the file does not exist: {file_bag_0}")
+logging.debug("- file_bag_0: %s" % file_bag_0)
 
 bag_0 = BAGFile(file_bag_0, mode="r")
 bag_0.populate_metadata()
-logger.debug(bag_0)
 
+logging.debug("- bag_0.metadata: %s" % bag_0.metadata())
